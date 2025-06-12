924c10b6c1a7ab4a0c3c66fb7e5f468455c54fd8
Mishel Sandoval <91763586+MishSR@users.noreply.github.com>
2025-06-12 16:07:57 -0600

    new

--- Game/server.mjs
+++ Game/server.mjs
@@ -29,3 +29,53 @@ inquire.prompt ([
         console.log('¡Felicidades! Escogiste la unica respuesta correcta');}
 });
     
+
+/*import Hello from './functions.js'
+import inquirer from 'inquirer';
+
+async function iniciar (){
+    let nombreValido= false;
+    let respuestas= {};
+}
+while (!nombreValido) {
+    const respuestaNombre = await inquirer.prompt ([
+    {
+        type: 'input',
+        name: 'nombre',
+        message: '¿Cuál es tu nombre?',
+},
+]);
+
+ if (!['Sherry', 'Karen', 'Itzel'].includes(respuestaNombre.nombre)) {
+        console.log('No puedes participar, gracias');
+    } else{
+        console.log('Hola mi amor, te estaba esperando');
+        nombreValido = true;
+        respuestas.nombre = respuestaNombre.nombre;
+    }
+}
+
+
+let estadoValido= false;
+while (!estadoValido){
+    const respuestaEstado = await inquirer.prompt ([
+        {
+    type: 'list',
+    message: '¿Cuál es tu cestado civil?',
+    name: 'status',
+    choices: ['Soltera', 'Casada', 'Divorciada', 'Viuda'],
+
+    },
+    ]);
+
+    if (respuestaEstado.status !=='Casada'){
+        console.log('Respuesta incorrecta, sigue intentando');
+    } else{
+        console.log('¡Felicidades! Escogiste la unica respuesta correcta');
+    estadoValido= true;
+    respuestas.status = respuestaEstado.status;
+}
+
+}
+
+iniciar();*/
